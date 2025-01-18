19c0452097df1ad74162354d5230492f06493abc
Suraj Pandey <142660897+Surraj70@users.noreply.github.com>
2025-01-18 16:29:05 +0000

    basics datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,10 @@
+// let value=3
+// let negValue=-value
+// console.log(negValue)
+
+
+console.log(2>1)
+console.log(5>3)
+console.log(3!=6)
+
+console.log('2'>1)
---
+++ 01_basics/04_datatypes.js
@@ -0,0 +1,8 @@
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+
+// node 01_basics/04_datatypes.js
+
+console.log(typeof(null))
