a31502f3b673cec56a989ae54b50753c136b254f
Suraj Pandey <142660897+Surraj70@users.noreply.github.com>
2025-01-21 15:15:29 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,13 @@
+const gameName = new String('hiteshhc')
+console.log(gameName[0])
+
+console.log(gameName.toUpperCase())
+console.log(gameName.length)
+
+const newString = '   suraj   '
+console.log(newString)
+console.log(newString.trim())
+
+
+const url = "https://suraj.com/suraj%20pandey/"
+console.log(url.replace('%20','-'))
