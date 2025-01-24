b38f13da13b65a13b7f0e6a2e73ca0dbc9ec35d9
Suraj Pandey <142660897+Surraj70@users.noreply.github.com>
2025-01-24 16:24:51 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,18 @@
+// const myArr = [0,1,2,3,4,5]
+
+// console.log(myArr.length)
+// const Arr = [1, 2, 3, [3, 4, 5], 4, 5]
+
+// console.log(Arr.flat())             //flat is a function it gives all subset of a array into a single array
+
+
+console.log(Array.isArray("Suraj"))       //to check whether the array is array or not
+
+console.log(Array.from("Suraj"))          //to convert into array
+
+score1=100
+score2=200
+score3=300
+
+
+console.log(Array.of(score1, score2, score3))
