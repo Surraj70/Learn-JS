4383bd2912cd832ccb81985c445280090322a9d6
Suraj Pandey <142660897+Surraj70@users.noreply.github.com>
2025-02-02 16:47:52 +0000

    loops

---
+++ 04_control_flow/two.js
@@ -0,0 +1,16 @@
+// for (let i = 1; i <= 10; i++) {
+//      console.log(`Outer Loop value : ${i}`);
+         
+//      for (let j = 1; j <= 10; j++) {
+//          console.log(`${i} * ${j} = ${i * j}`)
+//      }
+    
+// }
+
+
+// while loop
+let index = 0
+while (index < 10) {
+    console.log(`Value of index = ${index}`)
+    index = index + 2
+}
