881a644a03b422aed78653e581fc088fe5270509
Suraj Pandey <142660897+Surraj70@users.noreply.github.com>
2025-02-03 19:45:22 +0000

    .

---
+++ 04_control_flow/three.js
@@ -0,0 +1,15 @@
+//for of
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+const map = new Map()
+map.set('In', "India")
+map.set('Us', "America")
+map.set('NZ', "New Zealand")
+map.set('In', "India")
+
+console.log(map);
